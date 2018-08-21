e6e1c28de8e53b08091d204f0fe06f9ace22b0a1
Raphael Carubbi <rcarubbi@gmail.com>
2018-08-21 15:26:10 +0100

    Initial Commit

---
+++ Carubbi.Communication/NamedPipe/Unsubscriber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Carubbi.Communication.NamedPipe
+{
+    public  class Unsubscriber<TDtoOutput> : IDisposable
+        where TDtoOutput : class
+    {
+        private readonly List<IObserver<TDtoOutput>> _observers;
+        private readonly IObserver<TDtoOutput> _observer;
+
+        internal Unsubscriber(List<IObserver<TDtoOutput>> observers, IObserver<TDtoOutput> observer)
+        {
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observers.Contains(_observer))
+                _observers.Remove(_observer);
+        }
+    }
+}
